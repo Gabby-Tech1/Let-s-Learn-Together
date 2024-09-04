fea92696762a537fc885ded638e4023d7c832bcd
Gabby-Tech1 <gkaquainoo@gmail.com>
2024-09-04 16:39:27 +0000

    Resolved winner of circular game

--- Winner_of_circular_game.py
+++ Winner_of_circular_game.py
@@ -5,9 +5,15 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
+        array = []
         winner = 0
-        
-        for i in range(2, n+1):
-            winner = (winner + k) % i
 
-        return winner + 1
+        for i in range(1, n+1):
+            array.append(i)
+
+        while len(array) > 1:
+            winner = (winner + k - 1) 
+            winner = winner % len(array)
+            array.pop(winner)
+
+        return array[0]
